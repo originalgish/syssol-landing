62b7538bb752e95a0f7a57cf85c0d98242a24b74
Денис Магазейщиков <denismagazeyshchikov@Den.local>
2019-04-14 14:37:55 +0300

    lazy loading

--- src/components/Sections/Intro/index.jsx
+++ src/components/Sections/Intro/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import LazyLoad from 'react-lazy-load'
 
 import { intro } from '../../../data'
 
-import { IntroContainer, Title, SubTitle, HomeLink } from './styles'
+import { IntroContainer, Image, Title, SubTitle, HomeLink } from './styles'
+import introBG from './img/intro-bg.jpg'
 
 const Intro = () => (
   <IntroContainer>
+    <LazyLoad>
+      <Image src={introBG} alt="content image" />
+    </LazyLoad>
     <Title>{intro.title}</Title>
     <SubTitle>{intro.subtitle}</SubTitle>
     <HomeLink href="http://syssol.ru" target="_blank" rel="noopener noreferrer">
--- src/components/Sections/Intro/styles.js
+++ src/components/Sections/Intro/styles.js
@@ -10,14 +10,23 @@ export const IntroContainer = styled(SectionContainer)`
   flex-direction: column;
   align-items: center;
   height: 736px;
-  background-image: url(${introBG});
+  position: relative;
+  /* background-image: url(${introBG});
   background-size: cover;
   background-position: center;
-  background-repeat: no-repeat;
+  background-repeat: no-repeat; */
   @media ${device.mobileL} {
-    /* background-image: none; */
     height: 512px;
   }
+`
+export const Image = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
 `
 export const Title = styled.h1`
   margin: 0;
