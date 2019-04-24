c86c80ed09adde06344718a20980b33ffd2df672
Денис Магазейщиков <denismagazeyshchikov@Den.local>
2019-04-24 21:07:46 +0300

    hooks refactor

--- src/components/MenuBurger/index.jsx
+++ src/components/MenuBurger/index.jsx
@@ -1,42 +1,35 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import enhanceWithClickOutside from 'react-click-outside'
 
 import { Burger, Bar, LinksWrapper, TextLink } from './styles'
 
-class MenuBurger extends Component {
-  state = {
-    isOpen: false
+const MenuBurger = ({ headerLinksData }) => {
+  const [isOpen, setMenuState] = useState(false)
+
+  const hideMenu = () => setMenuState(false)
+
+  const toggleMenu = () => setMenuState(!isOpen)
+
+  const handleClickOutside = () => {
+    hideMenu()
   }
 
-  hideMenu = () => this.setState({ isOpen: false })
-
-  toggleMenu = () => this.setState({ isOpen: !this.state.isOpen })
-
-  handleClickOutside = () => {
-    this.hideMenu()
-  }
-
-  render() {
-    const { isOpen } = this.state
-    const { headerLinksData } = this.props
-
-    return (
-      <Burger onClick={this.toggleMenu} isOpen={isOpen} aria-label="menu">
-        <Bar isOpen={isOpen} />
-        <Bar isOpen={isOpen} />
-        <Bar isOpen={isOpen} />
-        {isOpen && (
-          <LinksWrapper>
-            {headerLinksData.links.map(link => (
-              <TextLink key={link.id} href={link.href}>
-                {link.text}
-              </TextLink>
-            ))}
-          </LinksWrapper>
-        )}
-      </Burger>
-    )
-  }
+  return (
+    <Burger onClick={toggleMenu} isOpen={isOpen} aria-label="menu">
+      <Bar isOpen={isOpen} />
+      <Bar isOpen={isOpen} />
+      <Bar isOpen={isOpen} />
+      {isOpen && (
+        <LinksWrapper>
+          {headerLinksData.links.map(link => (
+            <TextLink key={link.id} href={link.href}>
+              {link.text}
+            </TextLink>
+          ))}
+        </LinksWrapper>
+      )}
+    </Burger>
+  )
 }
 
 export default enhanceWithClickOutside(MenuBurger)
